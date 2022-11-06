f4c7f2ccc824f258e433e4ec61852fcfb62dc3b0
ikamrava <ikamrava>
2022-11-06 20:03:21 +0000

    Update java.js

--- java.js
+++ java.js
@@ -3,6 +3,20 @@ const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 myleads = [];
 
+storedData = JSON.parse(localStorage.getItem("key"));
+
+if (storedData) {
+  myleads = storedData;
+  renderList();
+}
+
+saveBtn.addEventListener("click", function () {
+  myleads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("key", JSON.stringify(myleads));
+  renderList();
+});
+
 function renderList() {
   let listItems = "";
   for (i = 0; i < myleads.length; i++) {
@@ -10,8 +24,3 @@ function renderList() {
   }
   ulEl.innerHTML = listItems;
 }
-saveBtn.addEventListener("click", function () {
-  myleads.push(inputEl.value);
-  inputEl.value = "";
-  renderList();
-});
